e3f411386ee2bafeda54ee5c7a897a8bb26a1911
PajiiMarr <151644959+PajiiMarr@users.noreply.github.com>
2026-01-18 22:14:36 +0800

    Anonymous functions

--- functions/anonymous_functions.go
+++ functions/anonymous_functions.go
@@ -4,9 +4,16 @@ import "fmt"
 
 func af() {
 	problemOneAf()
+	sliceVariable := []int {
+		1,2,3,4,
+	}
+	result := problemTwoAF(sliceVariable, func(n int) int {
+		return n * 2
+	})
+
+	fmt.Println(result)
 }
 
-// 3️⃣ Anonymous Functions
 // Challenge 1 (Easy)
 // Assign function to variable that doubles an integer
 // Call and print result
@@ -24,3 +31,14 @@ func problemOneAf() {
 // Challenge 2 (Medium)
 // Pass anonymous function as argument to another function
 // Apply function to slice of integers and print results
+
+
+
+func problemTwoAF(nums []int, op func(int) int ) []int {
+	result := []int{}
+	for _, n := range nums {
+		result = append(result, op(n))
+	}
+	return result
+}
+
