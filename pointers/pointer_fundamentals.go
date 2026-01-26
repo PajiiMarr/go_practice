ed9be2279796bd9902a552dc8d233cd8e4053e1a
PajiiMarr <151644959+PajiiMarr@users.noreply.github.com>
2026-01-26 21:39:38 +0800

    Pointer fundamentals progress

---
+++ pointers/pointer_fundamentals.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+func pf() {
+	// problemOnePf()
+	problemTwoPf()
+}
+
+// 🟢 EASY — Pointer Fundamentals
+// 1️⃣ Value vs Pointer Modification
+
+// Goal:
+// Demonstrate the difference between passing a value and passing a pointer to a function.
+
+// Hints:
+
+// Pass an integer two ways
+// Modify inside function
+// Compare results
+
+func modifyByValue(x int) {
+	x = 765
+}
+
+func modifyByPointer(x *int) {
+	*x = 765
+}
+
+func problemOnePf() {
+
+	x := 896
+	fmt.Println(x)
+
+	modifyByValue(x)
+	fmt.Println(x)
+
+	modifyByPointer(&x)
+	fmt.Println(x)
+}
+
+// 2️⃣ Swap Two Values
+
+// Goal:
+// Write a function that swaps two integers.
+
+// Hints:
+
+// Use pointers
+// No return values
+// Caller variables must change
+func problemTwoPointer(x *int, y *int) {
+	temp := *x
+	*x = *y
+	*y = temp
+}
+
+func problemTwoPf() {
+	x := 1
+	y := 5
+
+	fmt.Println("Before swap", x, ' ', y )
+	
+	problemTwoPointer(&x, &y)
+	fmt.Println("After swap", x, ' ' ,y)
+}
+
+
+// 3️⃣ Reset Struct Fields
+
+// Goal:
+// Write a function that resets all fields of a struct to default values.
+
+// Hints:
+
+// Pass struct by pointer
+
+// Modify fields directly
+
+// No struct copying
---
+++ pointers/pointers_main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	pf() // pointer_fundamentals.go
+}
